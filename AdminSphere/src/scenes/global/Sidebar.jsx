9605ae98eef0f61695811d77b3d27c112b9f7b3f
pranav1321-ops <pranavbhardwaj385@gmail.com>
2024-07-15 18:54:59 +0530

    Update Topbar

--- AdminSphere/src/scenes/global/Sidebar.jsx
+++ AdminSphere/src/scenes/global/Sidebar.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react"
+import {ProSidebar, Menu, MenuItem} from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme} from '@mui/material';
+import { Link } from "react-router-dom";
+import { tokens } from "../../themes";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactOutlinedIcon from "@mui/icons-material/ContactOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+
 const Sidebar =() =>{
     return <div>Sidebar</div>
 }
